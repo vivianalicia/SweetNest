ed1c8f5f6d65c066f311a743722998cd3da8dcb8
vivianalicia <vivian.alicia.va@gmail.com>
2025-05-22 12:03:23 +0700

    fixing style on product

--- js/home.js
+++ js/home.js
@@ -4,3 +4,20 @@ function scrollToSection() {
         heroSection.scrollIntoView({ behavior: 'smooth' });
     }
 }
+
+const fadeSections = document.querySelectorAll('.fade-section');
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target); 
+      }
+    });
+  }, {
+    threshold: 0.5
+  });
+
+  fadeSections.forEach(section => {
+    observer.observe(section);
+  });
